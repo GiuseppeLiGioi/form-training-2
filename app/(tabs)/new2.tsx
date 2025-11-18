492518b88ec0084519c00007e8c06a288f148eba
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-18 17:09:31 +0100

    deleted. content of new2Screen and start another logic. Guess the number

--- app/(tabs)/new2.tsx
+++ app/(tabs)/new2.tsx
@@ -1,9 +1,8 @@
 import { useAppContext } from "@/contexts/AppContext";
 import styles from "@/styles/common";
-import { FontAwesome5 } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
-import { Button, FlatList, ImageBackground, Text, View } from "react-native";
+import { ImageBackground } from "react-native";
 
 export default function New1Screen() {
   const [columns, setColumns] = useState<number>(2);
@@ -19,66 +18,7 @@ export default function New1Screen() {
         resizeMode="cover"
         style={styles.containerHome}
         imageStyle={styles.backgroundImage}
-      >
-        {personas.length === 0 ? (
-          <Text style={styles.topTextHome}>Nessuna persona da mostrare</Text>
-        ) : (
-          <View style={styles.containerFlatList}>
-            <FlatList
-              style={{ marginTop: 20 }}
-              contentContainerStyle={styles.flatList}
-              data={personas}
-              key={columns}
-              numColumns={columns}
-              columnWrapperStyle={{ justifyContent: "space-between" }}
-              renderItem={(itemData) => (
-                <LinearGradient
-                  colors={[
-                    "#4B3F92",
-                    "#8d7bf1ff",
-                    "#2BB3C7",
-                  ]} /*Messi entrambi GRADIENT + BACKGROUND SOLO PER USARLI ENTRAMBI IN UN SINGOLO PROGETTO */
-                  start={{ x: 0, y: 0 }}
-                  end={{ x: 1, y: 1 }}
-                  style={styles.containerSinglePerson}
-                >
-                  <View style={styles.containerAnagraphic}>
-                    <Text style={styles.nameCard}>{itemData.item.name}</Text>
-                    <Text style={styles.ageCard}>{itemData.item.age}</Text>
-                    <Text style={styles.employmentCard}>
-                      {itemData.item.employment}
-                    </Text>
-                  </View>
-
-                  <View style={styles.lineSeparatorCard}></View>
-
-                  <View style={styles.containerDetails}>
-                    <Text style={styles.isPatented}>
-                      {itemData.item.isPatented ? "Patentato" : "Non patentato"}
-                    </Text>
-                    <FontAwesome5
-                      name="car-side"
-                      size={16}
-                      color={itemData.item.isPatented ? "green" : "gray"}
-                    />
-                  </View>
-
-                  <View style={styles.lineSeparatorCard}></View>
-
-                  <View style={styles.buttonRemoveItem}>
-                    <Button
-                      title="Rimuovi"
-                      color="#fa4949ff"
-                      onPress={() => deletePersona(itemData.item.id)}
-                    />
-                  </View>
-                </LinearGradient>
-              )}
-              keyExtractor={(item) => item.id.toString()}
-            />
-          </View>
-        )}
-      </ImageBackground>
+      ></ImageBackground>
     </LinearGradient>
   );
 }
