a56fe501c7bcfba346446e98a50359995409a9f4
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-11-18 11:33:49 +0100

    restyled. other screens

--- app/(tabs)/new2.tsx
+++ app/(tabs)/new2.tsx
@@ -1,40 +1,51 @@
 import { useAppContext } from "@/contexts/AppContext";
 import styles from "@/styles/common";
-import { FlatList, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { FlatList, ImageBackground, Text, View } from "react-native";
 
 export default function New2Screen() {
   const { personas } = useAppContext();
 
   return (
-    <View style={styles.containerHome}>
-      {personas.length === 0 ? (
-        <Text style={styles.topTextHome}>Nessuna persona da mostrare</Text>
-      ) : (
-        <FlatList
-          contentContainerStyle={styles.flatList}
-          data={personas}
-          renderItem={(itemData) => (
-            <View style={styles.containerSinglePerson}>
-              <View style={styles.containerAnagraphic}>
-                <Text>{itemData.item.name}</Text>
-                <Text
-                  style={{
-                    color: itemData.item.isPatented ? "green" : "black",
-                  }}
-                >
-                  {itemData.item.isPatented ? "Patentato" : "Non patentato"}
-                </Text>
-              </View>
+    <LinearGradient
+      colors={["#2612ddff", "#dbdadaff"]}
+      style={styles.containerHome}
+    >
+      <ImageBackground
+        source={require("../../assets/images/background.jpg")}
+        resizeMode="cover"
+        style={[styles.containerHome, styles.backgroundImage]}
+      >
+        {personas.length === 0 ? (
+          <Text style={styles.topTextHome}>Nessuna persona da mostrare</Text>
+        ) : (
+          <FlatList
+            style={{ marginTop: 20 }}
+            contentContainerStyle={styles.flatList}
+            data={personas}
+            renderItem={(itemData) => (
+              <View style={styles.containerSinglePerson}>
+                <View style={styles.containerAnagraphic}>
+                  <Text>{itemData.item.name}</Text>
+                  <Text
+                    style={{
+                      color: itemData.item.isPatented ? "green" : "black",
+                    }}
+                  >
+                    {itemData.item.isPatented ? "Patentato" : "Non patentato"}
+                  </Text>
+                </View>
 
-              <View style={styles.containerDetails}>
-                <Text>{itemData.item.age}</Text>
-                <Text>{itemData.item.employment}</Text>
+                <View style={styles.containerDetails}>
+                  <Text>{itemData.item.age}</Text>
+                  <Text>{itemData.item.employment}</Text>
+                </View>
               </View>
-            </View>
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
-      )}
-    </View>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          />
+        )}
+      </ImageBackground>
+    </LinearGradient>
   );
 }
